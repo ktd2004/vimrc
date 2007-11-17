4a4cd81a63dbf7a32a1258858039e4f7103fa02d
TaeDong Kim <fly1004@gmail.com>
2007-11-17 12:45:36 +0000

    include guide에서 #define 값을 1로 설정함.

--- vimfiles/templates/skel.h
+++ vimfiles/templates/skel.h
@@ -1,7 +1,7 @@
 /*** DELETE ME :  auto make template file ****/
 /* vim:set ft=c ts=4 sw=4 sts=4 sta ai si ci bs=2: */
 #ifndef @INCLUDE_GAURD@
-#define @INCLUDE_GAURD@
+#define @INCLUDE_GAURD@	1
 
 
 
