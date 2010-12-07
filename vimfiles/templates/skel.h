f71f8b706abb9e3103c964461b018f04fff85d2c
TaeDong Kim <fly1004@gmail.com>
2010-12-07 07:53:38 +0000

    header파일 template의 filetype을 c에서 cpp로 변경함.

--- vimfiles/templates/skel.h
+++ vimfiles/templates/skel.h
@@ -1,5 +1,5 @@
 /*** DELETE ME :  auto make template file ****/
-/* vim:set ft=c ts=4 sw=4 sts=4 sta ai si ci bs=2: */
+/* vim:set ft=cpp ts=4 sw=4 sts=4 sta ai si ci bs=2: */
 #ifndef @INCLUDE_GAURD@
 #define @INCLUDE_GAURD@	1
 
