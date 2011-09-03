591e6c37602635ad9e18eafba03c57392e7e190a
TaeDong Kim <fly1004@gmail.com>
2011-09-03 02:26:07 +0000

    파일 template를 보기좋게 조금씩 수정함.

--- vimfiles/templates/skel.c
+++ vimfiles/templates/skel.c
@@ -1,7 +1,6 @@
 /* vim:set ft=c ts=4 sw=4 sts=4 sta ai si ci bs=2: */
 #include "@FILE@.h"
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
--- vimfiles/templates/skel.py
+++ vimfiles/templates/skel.py
@@ -9,3 +9,4 @@ import sys
 
 if __name__ == "__main__":
     pass
+
