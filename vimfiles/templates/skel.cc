158b2d2dbc8c94374150b8c6a3ee09495ee71ffc
TaeDong Kim <fly1004@gmail.com>
2011-12-22 05:39:39 +0000

    소스 파일에 기본으로 추가하는 헤더 파일이 없도록 함.
    필요한 경우에 include하는 것이 더 옳다.

--- vimfiles/templates/skel.cc
+++ vimfiles/templates/skel.cc
@@ -1,9 +1,6 @@
 /* vim:set ft=cpp ts=4 sw=4 sts=4 sta ai si ci bs=2: */
 #include "@FILE@.h"
 
-#include <cstdio>
-#include <iostream>
-
 
 
 int main(int /* argc */, char** /* argv */)
--- vimfiles/templates/skel.cpp
+++ vimfiles/templates/skel.cpp
@@ -1,9 +1,6 @@
 /* vim:set ft=cpp ts=4 sw=4 sts=4 sta ai si ci bs=2: */
 #include "@FILE@.h"
 
-#include <cstdio>
-#include <iostream>
-
 
 
 namespace @FILE@ {
