10dc3fe71605d110a132e9d160a9f6fca8773d60
TaeDong Kim <fly1004@gmail.com>
2013-02-01 18:08:11 +0900

    소스 파일의 template에 file comments 추가

--- vimfiles/templates/skel.cc
+++ vimfiles/templates/skel.cc
@@ -1,4 +1,18 @@
 /* vim:set ft=cpp ts=4 sw=4 sts=4 sta ai si cin bs=2 noet: */
+/**
+ * \file
+ * \brief
+ * \details
+ * \author NAME <EMAIL@gmail.com>
+ * \version
+ * \date @YEAR@-
+ * \copyright (C) @YEAR@- COMPANY, Inc.
+ * \copyright Apache 2.0, BSD, LGPL, GPL
+ * \note
+ * \bug
+ * \warning
+ * \todo
+ **/
 #include "@FILE@.h"
 
 
